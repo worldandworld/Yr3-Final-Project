a5a5ce9a65182d4f8e885a70355fd514ba0eeff4
abdulFs <abdul12x@live.ie>
2016-02-02 01:42:09 +0000

    ........

---
+++ Project/Yr3-SAD-Project/src/java/Accounting/UserHelper.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Accounting;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Abdul
+ */
+public class UserHelper {
+    Session session = null;
+
+    public UserHelper() {
+        this.session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+}
